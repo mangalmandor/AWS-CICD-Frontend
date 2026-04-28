e350cda219026e0128df04db00cc2d3962c93dfa
Mangal Mandor <mangal.mandor13102003@therapidhire.com>
2026-04-28 15:39:54 +0530

    updated frontend files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
                             onClick={() => navigate('/register')}
                             className="px-6 py-2.5 text-sm font-bold text-white bg-blue-600 rounded-full hover:bg-blue-500 transition-all shadow-[0_0_20px_rgba(37,99,235,0.3)] hover:shadow-[0_0_30px_rgba(37,99,235,0.5)] active:scale-95"
                         >
-                            Get Started
+                            Get Started !
                         </button>
                     </div>
                 </div>
