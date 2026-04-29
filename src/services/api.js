1487c08706980fd33988a54f3301466f2630debb
Mangal Mandor <mangal.mandor13102003@therapidhire.com>
2026-04-29 12:05:35 +0530

    changed backend endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://ec2-13-49-241-148.eu-north-1.compute.amazonaws.com:8080/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
